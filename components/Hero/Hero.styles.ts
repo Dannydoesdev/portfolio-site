401aa849b94a0d1fb7fcff594c7b07607063e760
Daniel McGee <daniel.t.mcgee@gmail.com>
2025-01-31 13:16:51 +0100

    Move to Mantine v6 - resolve breaking changes

--- components/Hero/Hero.styles.ts
+++ components/Hero/Hero.styles.ts
@@ -1,4 +1,4 @@
-import { createStyles } from '@mantine/core';
+import { createStyles, rem } from '@mantine/core';
 
 export default createStyles((theme) => ({
   root: {
@@ -6,12 +6,9 @@ export default createStyles((theme) => ({
     backgroundSize: 'contain',
     backgroundPosition: 'center',
     backgroundImage:
-      // 'linear-gradient(250deg, rgba(130, 201, 30, 0) 0%, #062343 70%), url(https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80)',
-      // 'linear-gradient(250deg, rgba(130, 201, 30, 0) 0%, #062343 70%), url(/img/hero-face.png)',
       'linear-gradient(250deg, rgb(63 100 10 / 0%) 0%, #0e0d0deb 70%),url(/img/hero/hero-face-bw-sml.jpg)',
-      // 'linear-gradient(250deg, rgba(130, 201, 30, 0) 0%, #062343 70%), url(/img/hero/hero-face-bw-sml.jpg)',
-    paddingTop: theme.spacing.xl * 10,
-    paddingBottom: theme.spacing.xl * 3,
+    paddingTop: `calc(${theme.spacing.xl} * 10)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 3)`,
   },
 
   inner: {
@@ -28,11 +25,10 @@ export default createStyles((theme) => ({
       display: 'none',
     },
   },
-
   content: {
-    paddingTop: theme.spacing.xl * 2,
-    paddingBottom: theme.spacing.xl * 2,
-    marginRight: theme.spacing.xl * 3,
+    paddingTop: `calc(${theme.spacing.xl} * 2)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 2)`,
+    marginRight: `calc(${theme.spacing.xl} * 3)`,
 
     [theme.fn.smallerThan('md')]: {
       marginRight: 0,
@@ -44,38 +40,33 @@ export default createStyles((theme) => ({
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
     fontWeight: 900,
     lineHeight: 1.05,
-    maxWidth: 500,
-    fontSize: 48,
+    maxWidth: rem(500),
+    fontSize: rem(48),
 
     [theme.fn.smallerThan('md')]: {
       maxWidth: '100%',
-      fontSize: 34,
+      fontSize: rem(34),
       lineHeight: 1.15,
     },
   },
 
   name: {
     lineHeight: 1.05,
-    fontSize: '150px',
+    fontSize: rem(150),
     backgroundImage: 'linear-gradient(45deg, #460376 0% 0%, #8700edf7 100% 100%)',
-    marginLeft: '-10px',
+    marginLeft: rem(-10),
 
     [theme.fn.smallerThan('md')]: {
       
-      fontSize: '110px',
-      marginLeft: '-5px',
-
-      // lineHeight: 1.15,
-      // backgroundImage: 'linear-gradient(45deg, #36005dfc 0% 0%, #8700edba 100% 100%)',
-
-      // Try -5px margin-left && 110 font-size
+      fontSize: rem(110),
+      marginLeft: rem(-5),
     },
   },
 
   description: {
     color: theme.white,
     opacity: 0.75,
-    maxWidth: 440,
+    maxWidth: rem(440),
 
     [theme.fn.smallerThan('md')]: {
       maxWidth: '100%',
@@ -83,10 +74,10 @@ export default createStyles((theme) => ({
   },
 
   control: {
-    paddingLeft: 50,
-    paddingRight: 50,
+    paddingLeft: rem(50),
+    paddingRight: rem(50),
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontSize: 22,
+    fontSize: rem(22),
 
     [theme.fn.smallerThan('md')]: {
       width: '100%',
